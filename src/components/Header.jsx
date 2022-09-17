1bfaa8df45a10c0055bb947e1f384553df50d894
jackjavi <jackmtembete@gmail.com>
2022-09-17 07:41:42 +0300

    Changed font and text for Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,11 @@ import Hero from "../Assets/hero.jpg";
 const Header = () => {
   return (
     <div className=" pt-24 md:pt-32 flex flex-col items-center justify-center h-full">
-      <p className="font-varela text-[#bcbe0c] md:absolute top-[24%] text-[12px] md:text-[20px]">
-        Sports News
+      <p className="font-signature text-[#bcbe0c] md:absolute top-[24%] text-[12px] md:text-[20px]">
+        Trending Trends
       </p>
       <p className="font-lora text-[#9e1b1b] md:absolute top-[25%] text-[45px] md:text-[100px]">
-        Blog
+        TRENDS
       </p>
       <img
         className="w-full h-[450px] object-cover mt-24 hidden md:block"
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        josefin: ['"Josefin Sans"', "sans-serif"],
-        lora: ["Lora", "serif"],
-        valera: ["Valera", "sans-serif"],
-      },
+    extend: {},
+    fontFamily: {
+      josefin: ['"Josefin Sans"', "sans-serif"],
+      lora: ["Lora", "serif"],
+      valera: ["Valera", "sans-serif"],
+      signature: ["Great Vibes"],
     },
   },
   plugins: [],

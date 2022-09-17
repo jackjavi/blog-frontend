457b56825b19d6891b169d63a3b75f2e67e11016
jackjavi <jackmtembete@gmail.com>
2022-09-17 11:27:08 +0300

    Animated Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import Hero from "../Assets/hero.jpg";
+import { useSpring, animated } from "react-spring";
 
 const Header = () => {
+  const styles = useSpring({
+    loop: true,
+    to: [
+      { opacity: 1, color: "#ffaaee" },
+      { opacity: 0, color: "rgb(14,26,19)" },
+    ],
+    from: { opacity: 0, color: "red" },
+  });
+
   return (
     <div className=" pt-24 md:pt-32 flex flex-col items-center justify-center h-full">
       <p className="font-signature text-[#bcbe0c] md:absolute top-[24%] text-[12px] md:text-[20px]">
         Trending Trends
       </p>
-      <p className="font-lora tranform hover:scale-125 ease-in-out  transition-all delay-125 text-[#9e1b1b] md:absolute top-[25%] text-[45px] md:text-[100px]">
+      <animated.p
+        style={styles}
+        className="font-lora tranform hover:scale-125 ease-in-out  transition-all delay-125 text-[#9e1b1b] md:absolute top-[25%] text-[45px] md:text-[100px]"
+      >
         TRENDS
-      </p>
+      </animated.p>
       <img
         className="w-full h-[450px] object-cover mt-24 hidden md:block"
         src={Hero}
